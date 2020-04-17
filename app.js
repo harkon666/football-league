8f165156d99649c7a14f6c88c33d7f57b445de00
harkon666 <bryan.wicaksanaa@gmail.com>
2020-04-18 00:09:32 +0700

    initialize app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const port = process.env.PORT;
 
 app.use(cors);
-app.listen(process.env.PORT);
+
+//import routes here
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
+
+//use routes here
+app.use("/api");
+
+//listen port here
+console.log(`great you have connected, started on port: ${port}`);
+app.listen(port);
